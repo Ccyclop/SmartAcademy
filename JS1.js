a1c9b512d4e5ae3d3fbf1217ca3e8816de51ac80
Tengiz Mikautadze <tengiz.mikautadze0@gmail.com>
2025-11-30 01:19:35 +0400

    Create JS1.js

---
+++ JS1.js
@@ -0,0 +1,53 @@
+// 1
+let x = 87;
+let y = 65;
+console.log(x + y);
+
+// 2
+let student = {
+  firstName: "Tegi",
+  lastName: "Mikautadze",
+  age: 20,
+  email: "tengiz.mikautadze0@gmail.com",
+  marks: 100
+};
+console.log(student.firstName);
+
+// 3
+let m = 45;
+let n = 23;
+
+if (m > n) {
+  console.log(m + n);
+} else {
+  console.log(n);
+}
+
+// 4
+let studentName = "Tegi";
+console.log(`Rise and shine, ${studentName} !`);
+
+// 5) bonus და salary: თუ salary >= 2000 → bonus = 0, თუ ქვევით → 10%
+let salary = 1800;
+let bonus;
+
+if (salary >= 2000) {
+  bonus = 0;
+} else {
+  bonus = salary * 0.1;
+}
+console.log(bonus);
+
+// 6
+let a = 26;
+let b = a / 2;
+let c = 2**a + b; // გაურკვეველია 2 ხარისხად (a-ს და b-ს ჯამს) გულისხმობთ თუ 2 ხარისხად a-ს და ცალკე b-ს ჯამს
+console.log(c);
+
+// 7
+let phones = ["Iphone", "Samsung", "Pixel", "Huawei", "Xiaomi"];
+console.log(phones);
+
+// 8
+console.log(phones[0]);
+console.log(phones[phones.length - 1]);
